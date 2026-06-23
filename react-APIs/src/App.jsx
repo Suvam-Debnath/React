dc3a55df565ad06ac84801fd4cf144a40bdc829c
Suvam Debnath <debnathsuvam322@gmail.com>
2026-06-23 21:09:53 +0530

    working with APIs

---
+++ react-APIs/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './App.css'
+import { useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  const[loading, setLoading] = useState(false);
+  const[error, setError] = useState(null);
+
+  useEffect(()=>{
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => {
+        setData(json)
+        setLoading(false);
+        //throw new Error('Something went wrong');
+      })
+      .catch((error) => {
+          console.log('Error fetching data: ', error);
+          setError('Failed to set data');
+          setLoading(false);
+      });
+  },[]);
+
+  if(loading){
+    return <p>Loading....</p>
+  }
+
+  if(error){
+    return <p>{error}</p>
+  }
+
+  return (
+   <div>
+    <h2>API's</h2>
+    <ul>
+        {data.map(post => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+   </div>
+  )
+}
+
+export default App
